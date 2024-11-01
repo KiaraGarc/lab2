1d681a1a2b0fc3eb339b02fe17dc690cd6831e94
KiaraGarc <kiaramarlengarcia@gmail.com>
2024-11-01 15:08:01 -0500

    Proyecto base
    Lab1 sin modificaciones

---
+++ chatbotl1/chatbot/src/main/java/com/mycompany/chatbot/Chatbot.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chatbot;
+
+/**
+ *
+ * @author Kiara García
+ */
+public class Chatbot {
+
+    public static void main(String[] args) {
+        Buscar frame= new Buscar();
+        frame.setVisible(true);
+        frame.setSize(764, 465);
+    }
+}
