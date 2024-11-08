c6d6833df481efc2f42a267947d63aca9b9a8d56
KiaraGarc <kiaramarlengarcia@gmail.com>
2024-11-08 13:03:42 -0500

    Revert "Update Buscar.java"
    This reverts commit ba52b7aced7d481588d23bdba7d7ee0c4d55906a.

--- chatbotl1/chatbot/src/main/java/com/mycompany/chatbot/Buscar.java
+++ chatbotl1/chatbot/src/main/java/com/mycompany/chatbot/Buscar.java
@@ -261,7 +261,6 @@ public class Buscar extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Buscar().setVisible(true);
-                System.out.println("po");
             }
         });
     }
