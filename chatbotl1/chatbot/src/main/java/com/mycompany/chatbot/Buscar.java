2cf657209bc4b8b4e545495befc2ae25c604501a
KiaraGarc <kiaramarlengarcia@gmail.com>
2024-11-08 13:38:38 -0500

    se arreglo

--- chatbotl1/chatbot/src/main/java/com/mycompany/chatbot/Buscar.java
+++ chatbotl1/chatbot/src/main/java/com/mycompany/chatbot/Buscar.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.mycompany.chatbot;
 
 import java.io.BufferedReader;
@@ -197,6 +193,7 @@ public class Buscar extends javax.swing.JFrame {
             System.out.println("Excepcion capturada: " + e.getMessage());
         }
   
+           txtpregunta.setText("");
         
         
 
